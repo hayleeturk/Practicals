814917ba5be9040d023b9dcd31d83000996b1619
haylee <haylee.turk@my.jcu.edu.au>
2022-09-26 20:21:01 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,11 +1,24 @@
+"""CP1404 Practical
+Password verification program with functions
+"""
+
 
 def main():
     """Program to verify password length"""
     minimum = 10
-    password_attempt = input("Enter password: ")
+    password_attempt = get_password()
     while len(password_attempt) < minimum:
         print("Password must be at least 10 characters")
         password_attempt = input("Enter password: ")
+    print_asterisks(password_attempt)
+
+
+def get_password():
+    password_attempt = input("Enter password: ")
+    return password_attempt
+
+
+def print_asterisks(password_attempt):
     for i in range(len(password_attempt)):
         print("*", end="")
 
