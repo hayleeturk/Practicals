5970a4c02d5f012e4133523683aff30f974d4f4a
haylee <haylee.turk@my.jcu.edu.au>
2022-11-04 09:15:01 +1000

    Add completed programming languages program

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,19 +5,20 @@ Create programming language class
 
 
 class ProgrammingLanguage:
+    """Represent a Programming Language object."""
 
     def __init__(self, name="", typing=bool, reflection="", year=0):
-        """Initiate programming language class objects"""
+        """Initialise a Programming Language instance. """
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
-        """Return data as a string"""
+        """Return data as a formatted f string."""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
-        """Determine if language typing is dynamic"""
+        """Determine if language typing is dynamic."""
         if self.typing == "Dynamic":
             return True
