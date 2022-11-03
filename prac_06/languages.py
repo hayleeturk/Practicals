ec9ccfab2c299b703f6f22d8bdbb2a1fff47f1d7
haylee <haylee.turk@my.jcu.edu.au>
2022-11-04 00:40:42 +1000

    Add programming languages class

---
+++ prac_06/languages.py
@@ -0,0 +1,15 @@
+class ProgrammingLanguage:
+
+    def __init__(self, language="", typing=bool, reflection="", year=0):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        if self.typing:
+            return "Dynamic"
+        return "Static"
---
+++ prac_06/programming_language.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practical 6
+Programming languages
+Estimated time: 20 minutes
+Actual time:
+"""
+
+from prac_06.languages import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+
+
+
+
+
