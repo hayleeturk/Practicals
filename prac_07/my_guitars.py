5738eefe759c3c466e9b2df58f4ce54280ed4083
haylee <haylee.turk@my.jcu.edu.au>
2022-11-11 23:20:25 +1000

    Add starter files

---
+++ prac_07/my_guitars.py
@@ -0,0 +1,11 @@
+"""
+CP1404 Practical
+"""
+
+from prac_07 import guitars.csv
+from prac_06 import guitar
+
+def main():
+    """Program to read from file and store data in list using class."""
+    in_file =
+for line in
