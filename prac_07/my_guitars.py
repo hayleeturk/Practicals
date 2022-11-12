79d978f7229ab690d3459a930f5ba55746b2d29b
haylee <haylee.turk@my.jcu.edu.au>
2022-11-12 13:07:28 +1000

    Add updates to read csv and store list using guitar class

--- prac_07/my_guitars.py
+++ prac_07/my_guitars.py
@@ -2,10 +2,27 @@
 CP1404 Practical
 """
 
-from prac_07 import guitars.csv
-from prac_06 import guitar
+from prac_06.guitar import Guitar
+
+FILENAME = "guitars.csv"
+
 
 def main():
     """Program to read from file and store data in list using class."""
-    in_file =
-for line in
+    in_file = open(FILENAME, "r")
+    my_guitars = []
+    for line in in_file:
+        parts = line.strip().split(",")  # Split data at ","
+        name = parts[0]
+        year = parts[1]
+        cost = parts[2]
+        my_guitar = Guitar(name, year, cost)
+        my_guitars.append(my_guitar)
+    in_file.close()
+
+    for my_guitar in my_guitars:  # testing
+        print(my_guitar)
+
+
+if __name__ == "__main__":
+    main()
