98cdc1f12ab3d836a573d0d6a237a44da0e352a1
haylee <haylee.turk@my.jcu.edu.au>
2022-11-20 22:31:06 +1000

    Add display projects function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,10 +5,12 @@ Estimated time:
 Actual time:
 """
 
+from prac_07.project import Project
 import datetime
 
 MENU = "(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects by date\n(A)dd new project\n(U)pdate " \
        "project\n(Q)uit "
+FILENAME = "projects.txt"
 
 
 def main():
@@ -21,7 +23,7 @@ def main():
         elif menu_choice == "S":
             pass
         elif menu_choice == "D":
-            pass
+            display_projects(FILENAME)
         elif menu_choice == "F":
             pass
         elif menu_choice == "A":
@@ -29,4 +31,29 @@ def main():
         elif menu_choice == "U":
             pass
         print(MENU)
-    menu_choice = input(">>>").upper()
+        menu_choice = input(">>>").upper()
+    print("Finished")
+
+
+def display_projects(filename):
+    project = []
+    in_file = open(filename, "r")
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split("\t")
+        name = parts[0]
+        date = parts[1]
+        priority = parts[2]
+        cost = parts[3]
+        percentage = parts[4]
+        my_project = Project(name, date, priority, cost, percentage)
+        project.append(my_project)
+    print(project)
+    in_file.close()
+
+
+
+
+
+
+main()
