c28612789fd4e238012dea2531c1373401caa940
hayleeturk <100130458+hayleeturk@users.noreply.github.com>
2023-01-13 15:16:23 +1000

    Update program with functions

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -9,29 +9,24 @@ from prac_07.project import Project
 import datetime
 from operator import attrgetter
 
-FILENAME = "projects.txt"
+# FILENAME = "projects.txt"
 MENU = "(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects by date\n(A)dd new project\n(U)pdate " \
        "project\n(Q)uit"
-
-
-def save_projects(projects):
-    """Save projects to file."""
-    with open("savedpro.txt", "w", encoding="utf-8") as out_file:
-        for project in projects:
-            print(project.name, project.date, project.priority, project.cost, project.percentage)
 
 
 def main():
     """Load and save a data file and use a list of Project objects."""
     print(MENU)
     menu_choice = input(">>>").upper()
-    projects = load_projects(FILENAME)
+    projects = []
     while menu_choice != "Q":
-        if menu_choice == "S":
+        if menu_choice == "L":
+            filename = input("Enter file name: ")
+            projects = load_projects(filename, projects)
+        elif menu_choice == "S":
             save_projects(projects)
         elif menu_choice == "D":
             projects.sort(key=attrgetter("date"))
-            print(projects)
             completed_projects = determine_status(projects)
             display_projects(projects, completed_projects)
         elif menu_choice == "F":
@@ -45,17 +40,16 @@ def main():
     print("Finished")
 
 
-def load_projects(filename):
+def load_projects(filename, projects):
     """Load projects from specified file."""
-    projects = []
     with open(filename, "r", encoding="utf-8") as in_file:
         in_file.readline()
         for line in in_file:
             parts = line.strip().split("\t")
             name = parts[0]
             date = parts[1]
-            priority = parts[2]
-            cost = parts[3]
+            priority = int(parts[2])
+            cost = float(parts[3])
             percentage = parts[4]
             my_project = Project(name, date, priority, cost, percentage)
             projects.append(my_project)
@@ -86,12 +80,10 @@ def get_date():
     """Get start date from user and return formatted date. """
     date_choice = input("Set start date as today? (Y/N): ").upper()
     if date_choice == "Y":
-        date_str = datetime.datetime.now()
+        date = datetime.datetime.now()
     else:
-        date_str = input("Start date (d/m/yyyy): ")  # e.g., "30/9/2022"
-    date = str_to_date(date_str)
+        date = input("Start date (d/m/yyyy): ")  # e.g., "30/9/2022"
     return date
-
 
 def get_new_project(projects):
     """Take user input to add new project."""
@@ -111,34 +103,47 @@ def update_project(projects):
     project_choice = int(input("Project choice: ")) - 1
     print(projects[project_choice])
     new_percentage = int(input("New percentage: "))
-    projects[project_choice][4] = new_percentage
+    while new_percentage == "":
+        new_percentage = int(input("New percentage: "))
+    projects[project_choice].percentage = new_percentage
     new_priority = int(input("New priority: "))
-    if new_priority != "":
-        projects[project_choice][2] = new_priority
+    while new_priority == "":
+        new_priority = int(input("New priority: "))
+    projects[project_choice].priority = new_priority
+
+
+def save_projects(projects):
+    """Save projects to chosen file."""
+    filename = input("Enter file name: ")
+    with open(filename, "w", encoding="utf-8") as out_file:
+        print(f"Name\tStart\tDate\tPriority\tCost\tEstimate\tCompletion\tPercentagetest", file=out_file)  # header
+        for project in projects:
+            print(f"{project.name}\t{project.date}\t{project.priority}\t{project.cost}\t{project.percentage}",
+                  file=out_file)
 
 
 def filter_by_date(projects):
     """Display only projects that start after a specified date, sorted by date."""
-    date_str = input("Show projects that start after date (dd/mm/yy): ")
-    filtered_date = str_to_date(date_str)
+    date = input("Show projects that start after date (dd/mm/yy): ")
     for project in projects:
-        date = str_to_date()
-    # filtered_projects = [project for project in projects if project.date > filtered_date]
+        date_objects = str_to_date(project.date)
+        # date_object = str_to_date(date)
+    filtered_projects = [project for project in projects if date_objects > date]
     print(filtered_projects)
-    print(sorted(filtered_projects, key=attrgetter("date")))
+    # print(sorted(filtered_projects, key=attrgetter("date")))
     #     if date > filtered_date:
     #         print(project)
 
 
-def str_to_date(date_str):
+def str_to_date(date):
     """Convert date string to date object."""
-    date = datetime.datetime.strptime(date_str, "%d/%m/%Y").date()
-    return date
+    date_object = datetime.datetime.strptime(date, "%d/%m/%y").date()
+    return date_object
 
 
 def date_to_str(date):
     """Convert date object to string."""
-    return date.strftime("%d/%m/%Y")
+    return date.strftime("%d/%m/%y")
 
 
 main()
